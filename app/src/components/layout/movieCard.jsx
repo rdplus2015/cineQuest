e4c99712b5084e666235946f45ad99ffbc0a6b7e
rdplus2015 <rdplus2015@gmail.com>
2025-06-30 07:52:57 -0400

    movieCard added

--- app/src/components/layout/movieCard.jsx
+++ app/src/components/layout/movieCard.jsx
@@ -5,7 +5,7 @@ export default function MovieCard({title, year, imgUrl}) {
                         <img src={imgUrl} alt={title}  className="w-full h-64 object-cover "/>
                         <div className="p-4 text-center text-white">
                                 <h3 className="text-[clamp(1rem,2vw,1.5rem)] font-semibold "> {title}</h3>
-                                <p className="text-[clamp(0.75rem,1.5vw,1rem)] text-gray-400 mt-1"> {year} </p>
+                                <p className="text-[clamp(0.75rem,1.5vw,1rem)] text-gray-400 mt-1 bg-mainBlack "> {year} </p>
                         </div>
                 </div>
         )
