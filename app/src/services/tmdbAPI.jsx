9f9d1a9b50d26a7fd0d901e9469f79c07e2a96cb
rdplus2015 <rdplus2015@gmail.com>
2025-07-04 20:33:22 -0400

    api service added

---
+++ app/src/services/tmdbAPI.jsx
@@ -0,0 +1,14 @@
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY 
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export async function getPopularMovies({page}) {
+        const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${page}`)
+        const data = await res.json()
+        return data;
+}
+
+export async function getMovie({movieId}){
+        const res = await fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`)
+        const data = await res.json();
+        return data;
+}
