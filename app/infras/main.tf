7ca3632a7b2c1d854ce4507bd921aba27b37dd60
rdplus2015 <rdplus2015@gmail.com>
2025-07-09 17:09:39 -0400

    terraform added and s3 bucket and policies created

---
+++ app/infras/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+        region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "site" {
+        bucket = "cinequest-tf"
+}
+
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.site.arn}/*"
+      }
+    ]
+  })
+}
